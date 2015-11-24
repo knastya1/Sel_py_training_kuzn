fba6937c2626c6933e0940ddf0b6ffa9324b24be
knastya <knastya@yandex-team.ru>
2015-11-25 02:39:35 +0300

    Lesson2, task4 Search Test (better vers) tests   and modification in process

---
+++ php4dvd/knastya_selen_fixture.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import pytest
+from model.applic import Application
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import *
+
+
+@pytest.fixture
+def app(request):
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    wait = WebDriverWait(driver, 30)
+    request.addfinalizer(driver.quit)
+    return Application(driver,wait)
---
+++ php4dvd/model/applic.py
@@ -0,0 +1,7 @@
+
+
+class Application(object):
+
+    def __init__(self, driver, wait):
+        self.driver = driver
+        self.wait=wait
